f0c09be192185b8ed38b9d9b063f71fc974b2eb2
almaz7 <almaz.idiyatullin7@gmail.com>
2022-04-18 23:14:10 +0300

    id_filter task is in testing stage

--- almaz/id_filter.py
+++ almaz/id_filter.py
@@ -1,12 +1,13 @@
 import openpyxl
 
-Exp_book = openpyxl.open("D:/py/bot/EXP_Бот.xlsx")
-sheet_0 = Exp_book.active
-column_B = sheet_0['B']
-
 
 def id_filter(id):
+    Exp_book = openpyxl.open("D:/py/bot/EXP_Бот.xlsx")
+    sheet_0 = Exp_book.active
+    column_B = sheet_0['B']
     for i in range(2, len(column_B) + 1):
         if id == sheet_0[i][0].value:
+            Exp_book.close()
             return True
+    Exp_book.close()
     return False
